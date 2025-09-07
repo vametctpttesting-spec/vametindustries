255ea590d53432f08e67a803d38f082efe9dffeb
Saicharan Poduri <thegrihome@gmail.com>
2025-09-06 20:06:55 -0700

    change vendors and files

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -1,6 +1,13 @@
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Sponsors() {
+  const [selectedPdf, setSelectedPdf] = useState(null)
+
+  const handleImageClick = pdfPath => {
+    window.open(pdfPath, '_blank')
+  }
+
   return (
     <section className="pb-6 pt-8" id="sponsors">
       <div className="px-4 pb-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -12,41 +19,41 @@ export default function Sponsors() {
           TSNPDCL for supplying Current Transformers(CT) and Potential
           Transformers (PT)
         </p>
-        <div className="max-w-4xl mx-auto grid grid-cols-2 gap-2 mt-6 md:grid-cols-4 md:gap-3">
-          <div className="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
+        <div className="max-w-4xl mx-auto grid grid-cols-3 gap-4 mt-6">
+          <div className="flex justify-center col-span-1">
             <Image
-              src="/images/site/vamet-vr-1.jpg"
+              src="/images/site/vamet-vr-1.jpeg"
               alt="APSPDCL Vendor Registration"
               width={200}
               height={120}
-              className="mt-6 h-16 w-auto object-contain transition duration-300 hover:scale-105"
+              className="mt-6 h-16 w-auto object-contain transition duration-300 hover:scale-105 cursor-pointer"
+              onClick={() =>
+                handleImageClick('/images/site/vamet-vr-1-page.pdf')
+              }
             />
           </div>
-          <div className="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
+          <div className="flex justify-center col-span-1">
             <Image
-              src="/images/site/vamet-vr-2.jpg"
+              src="/images/site/vamet-vr-2.jpeg"
               alt="APTRANSCO Vendor Registration"
               width={200}
               height={120}
-              className="mt-6 h-16 w-auto object-contain transition duration-300 hover:scale-105"
+              className="mt-6 h-16 w-auto object-contain transition duration-300 hover:scale-105 cursor-pointer"
+              onClick={() =>
+                handleImageClick('/images/site/vamet-vr-2-page.pdf')
+              }
             />
           </div>
-          <div className="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
+          <div className="flex justify-center col-span-1">
             <Image
-              src="/images/site/vamet-vr-3.jpg"
+              src="/images/site/vamet-vr-3.jpeg"
               alt="TSNPDCL Vendor Registration"
               width={200}
               height={120}
-              className="mt-6 h-16 w-auto object-contain transition duration-300 hover:scale-105"
-            />
-          </div>
-          <div className="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
-            <Image
-              src="/images/site/vamet-vr-4.jpg"
-              alt="Vendor Registration Certificate"
-              width={200}
-              height={120}
-              className="mt-6 h-16 w-auto object-contain transition duration-300 hover:scale-105"
+              className="mt-6 h-16 w-auto object-contain transition duration-300 hover:scale-105 cursor-pointer"
+              onClick={() =>
+                handleImageClick('/images/site/vamet-vr-3-page.pdf')
+              }
             />
           </div>
         </div>
